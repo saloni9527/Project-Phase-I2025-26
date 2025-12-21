227676c59fd003ea921f7bb84c5b472eed4189de
Saloni Navgire <165890821+saloni9527@users.noreply.github.com>
2025-12-21 12:24:17 +0530

    Delete Shrikrushna_214_BtechB_AI_DRIVEN_SMART_SEEDING directory

--- Shrikrushna_214_BtechB_AI_DRIVEN_SMART_SEEDING/Project/ml-backend/app_debug.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-try:
-    df = pd.read_csv('e:/Smart seeding/Smart seeding/ml-backend/dataset/village_crop_yield_2000.csv')
-    with open('cols.txt', 'w') as f:
-        for col in df.columns:
-            f.write(col + '\n')
-    print("Done")
-except Exception as e:
-    print(e)
